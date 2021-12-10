7d1aae69de23f28a77c1fe0d77685e76b96c1703
araff-16 <alex_raffa16@hotmail.com>
2021-12-10 18:30:37 -0500

    iplement back function in useVisualMode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,11 +3,20 @@ import {useState} from "react";
 export default function useVisualMode(initial) {
 
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
   const transition = (value) => {
+    setHistory([...history, value])
     setMode(value)
   }
 
-  return {mode, transition};
+  const back = () => {
+    if(history.length > 1){
+    setMode(history[history.length-2]) 
+    setHistory(history.slice(0,-1))
+    }
+  }
+ 
+  return {mode, transition, back};
 }
 
