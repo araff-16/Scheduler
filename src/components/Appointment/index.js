0d30c1fdf794d1b5d78a676a78c1a7b1b27de4fc
araff-16 <alex_raffa16@hotmail.com>
2021-12-10 19:22:32 -0500

    added passed the back function for onCancel for the Form componenet

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -30,7 +30,7 @@ export default function Appointment(props) {
           interviewer={props.interview.interviewer}
         />
       )}
-      {mode=== CREATE && <Form interviewers={[]}/>}
+      {mode=== CREATE && <Form interviewers={[]} onCancel={back}/>}
     </article>
   )
 }
