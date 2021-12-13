a936cab866cfb2df8bac1f47ff7e741646ff3d38
araff-16 <alex_raffa16@hotmail.com>
2021-12-13 10:22:08 -0500

    fixed the interviewlistitem story to properly listen for clicks

--- stories/index.js
+++ stories/index.js
@@ -122,7 +122,7 @@ storiesOf("InterviewerListItem", module)
       id={interviewer.id}
       name={interviewer.name}
       avatar={interviewer.avatar}
-      setInterviewer={action("setInterviewer")}
+      setInterviewer={() => action("setInterviewer")(interviewer.id)}
     />
   ));
 
