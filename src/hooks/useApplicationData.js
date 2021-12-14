67845d85954fb2b2469fe65e9ee3effdda0e58a1
araff-16 <alex_raffa16@hotmail.com>
2021-12-14 14:35:06 -0500

    revision to the spots counting state

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,22 +1,22 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 import axios from "axios";
 
 export default function useApplicationData() {
 
-  const [state,setState] = useState(
+  const [state, setState] = useState(
     {
-      days:[],
+      days: [],
       day: 'Monday',
       appointments: {},
       interviewers: {}
     }
-  ) 
+  )
 
   const setDay = day => setState({ ...state, day });
-  const setDays = days => setState(prev => ({...prev, days }));
+  const setDays = days => setState(prev => ({ ...prev, days }));
 
-  useEffect(()=> {
+  useEffect(() => {
     Promise.all([
       axios.get('/api/days'),
       axios.get('/api/appointments'),
@@ -26,9 +26,37 @@ export default function useApplicationData() {
       console.log('SECOND', all[1].data); // second
       console.log('THIRD', all[2].data);
 
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     });
-  } ,[])
+  }, [])
+
+
+  function updateSpots(id, condition) {
+    const existingState = { ...state }
+
+
+    const daySpotsChange = existingState.days.find((day) => day.appointments.includes(id))
+
+    const daySpotsIndex = existingState.days.findIndex((day) => day.id === daySpotsChange.id)
+
+    console.log("daySpotsChange", daySpotsChange)
+
+    console.log("daySpotsIndex", daySpotsIndex)
+
+
+    if (condition === "ADD") {
+      existingState.days[daySpotsIndex].spots ++
+    }else {
+      existingState.days[daySpotsIndex].spots --
+    }
+    console.log ("existingState", existingState)
+
+
+    setState(prev => ({ ...prev, ...existingState}));
+
+  }
+  //updateSpots()
+
 
   //USING PROMISES 
   function bookInterview(id, interview) {
@@ -41,21 +69,33 @@ export default function useApplicationData() {
       [id]: appointment
     };
 
-    return axios.put(`/api/appointments/${id}`, {interview})
-    .then (() => setState({ ...state, appointments:appointments}))
-     
+    return axios.put(`/api/appointments/${id}`, { interview })
+    .then(() => {
+      // setState({ ...state, appointments: appointments })
+      updateSpots(id, "REMOVE")
+      setState(prev => ({ ...prev, appointments: appointments }))
+      
+    })
+
   }
-    
+
   //OR ASYNC AWAIT CAN BE USED
   //
-  async function cancelInterview(id){
-    const updateAppointments = {...state}
+  function cancelInterview(id) {
+    const updateAppointments = { ...state }
 
     updateAppointments.appointments[id].interview = null;
 
-    await axios.delete(`/api/appointments/${id}`)
-    setState(updateAppointments)
+    return axios.delete(`/api/appointments/${id}`)
+    .then(() => {
+      setState(updateAppointments)
+      updateSpots(id, "ADD")
+    })
   }
-  
-  return {state, setDay, bookInterview, cancelInterview}
+
+
+
+
+
+  return { state, setDay, bookInterview, cancelInterview }
 }
