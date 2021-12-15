9e171fd70afe216f0db2f478e9e7763b4665dd08
araff-16 <alex_raffa16@hotmail.com>
2021-12-15 16:18:46 -0500

    cypress testing for dayList selection

--- cypress/integration/navigations.spec.js
+++ cypress/integration/navigations.spec.js
@@ -1,5 +1,34 @@
 describe("Navigation", () => {
+
   it("should visit root", () => {
     cy.visit("/");
   });
+
+  it("should nvaigate to Tuesday", () => {
+    cy.visit("/")
+    
+    // cy.get(".sidebar__menu ul,")
+    // .contains('Tuesday')
+    // .click()
+    // cy.contains("li", "Tuesday").should("have.css", "background-color", "rgb(242, 242, 242)")
+
+    //REFACTOR 1
+    // cy.contains("li", "Tuesday")
+    // .click()
+    // .should("have.css", "background-color", "rgb(242, 242, 242)");
+
+    //REFACTOR 2
+    // cy.contains(".sidebar__menu ul li","Tuesday")
+    // .click()
+    // .should("have.css", "background-color", "rgb(242, 242, 242)")
+
+    //REFACTOR 3
+    cy.contains("[data-testid=day]","Tuesday")
+    .click()
+    .should("have.class", "day-list__item--selected")
+
+  });
+
+
+
 });
